5c8b15c58e439e2e5f43ac92aba9edff287ea2c1
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-07-20 23:52:19 +0530

    Fix database connection configs for render deployment

--- order-service/src/db/index.js
+++ order-service/src/db/index.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 const connectDB = async () => {
