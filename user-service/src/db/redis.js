825508a722eb9b0b6b74a0d92b9f741d344a174d
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-07-21 15:41:04 +0530

    user db fix

--- user-service/src/db/redis.js
+++ user-service/src/db/redis.js
@@ -3,7 +3,7 @@ const { Redis } = require('ioredis');
 // Shared Redis connection for user-service
 const redisClient = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
 
-redisClient.on('connect', () => console.log('📦 User Service connected to Redis'));
+redisClient.on('connect', () => console.log('User Service connected to Redis'));
 redisClient.on('error', (err) => console.error('Redis connection error in User Service:', err));
 
 module.exports = redisClient;
--- user-service/src/models/index.js
+++ user-service/src/models/index.js
@@ -2,7 +2,6 @@ const { sequelize } = require('../db/index');
 const User = require('./user.model');
 const RefreshToken = require('./refreshToken.model');
 
-// Setup relationships
 User.hasMany(RefreshToken, { onDelete: 'CASCADE' });
 RefreshToken.belongsTo(User);
 
